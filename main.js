a7e80d1fed042aed94f55ff012db9b8fc387e26e
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-07 14:22:45 +0900

    Data type / Readme파일

--- main.js
+++ main.js
@@ -12,7 +12,9 @@ var app = http.createServer(function(request,response){
     }
     response.writeHead(200);
     console.log(__dirname + url); //디렉토리 경로를 터미널에 보여준다
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(fs.readFileSync(__dirname + url));//경로에 대한 파일을 읽어서 가져온다
+    //사용자에게 전송할 데이터를 생성한다
  
 });
 app.listen(3000);
+
---
+++ syntax/number.js
@@ -0,0 +1,4 @@
+console.log(1+1);//이항연산자
+console.log(4-1);
+console.log(2*2);
+console.log(10/2);
