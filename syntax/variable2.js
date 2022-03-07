539c454117af9a166b1868d6bfa377503263cc8a
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-07 14:54:09 +0900

    변수의 활용

---
+++ syntax/variable2.js
@@ -0,0 +1,4 @@
+var name="egoing"
+var letter= "Dear "+name+" Lorem Ipsum is simply dummy text of the printing "+name+" and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,"+name+" but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets "+name+" containing Lorem Ipsum p "+name+" assages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+console.log(letter);
+//변수를 쓰면 코드의 중복을 줄이고 더 좋은 코드로 만들 수 있다
