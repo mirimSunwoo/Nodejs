44260678049bd56422ff3b8aa802f4180bfbc24b
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-14 12:04:40 +0900

    비교연산자

---
+++ syntax/comparison.js
@@ -0,0 +1,17 @@
+console.log(1+1);
+console.log(2);
+
+//비교 연산자
+console.log(1==1); //true
+console.log(1==2); //false
+
+console.log(1>2); //false
+console.log(1<2); //true
+
+//정확하게 같은지 확인하고 싶을때(이유가 없다면 3개를 쓰자)
+console.log(1===1); //true
+console.log(1===2); //false
+
+name = 1; //대입연산자 
+name == 1; //비교 연산자 
+name === 1; //똑같지만 이걸 쓰자
