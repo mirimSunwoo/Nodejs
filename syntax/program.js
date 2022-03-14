883e71b0e3d9db2757b9a0632c89dc23c68e7da8
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-14 15:13:08 +0900

    제어문

---
+++ syntax/program.js
@@ -0,0 +1,12 @@
+console.log('A');
+console.log('B');
+console.log('C1');
+console.log('D');
+
+console.log('A');
+console.log('B');
+console.log('C2');
+console.log('D');
+
+//제어문 조건문 예고
+//최소한의 도구를 이용해서 나의 문제를 해결하는
