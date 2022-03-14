76c471e8e9f8319fda88201499fec95431071ae8
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-14 11:42:18 +0900

    boolean

---
+++ syntax/boolean.js
@@ -0,0 +1,9 @@
+//Boolean datatype
+//Comparison operator : 비교 연산자
+//Conditional statement : 조건문 
+
+//true or fasle
+console.log(true);
+console.log(false);
+a = 1; 
+// true = 1; boolen의 데이터 타입이기 때문에 변수로 사용하지 못함
