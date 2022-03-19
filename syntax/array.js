f37919d7228398a34fd82d946c56890206816845
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-03-20 07:17:28 +0900

    배열

---
+++ syntax/array.js
@@ -0,0 +1,16 @@
+//CRUD
+var arr = ['A','B','C','D']; // 생성
+
+console.log(arr[1]); //인덱스 0부터 시작
+console.log(arr[3]); //불러오기
+
+arr[2] = 3; //변경
+console.log(arr);
+
+console.log(arr.length); //데이터의 갯수
+
+arr.push('E'); //데이터 추가
+console.log(arr);
+
+
+
