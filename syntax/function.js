cb8bce0034659a9165b9ff297da03071a56518d5
mirimSunwoo <w2010@e-mirim.hs.kr>
2022-03-21 15:10:16 +0900

    함수의 기본 문법

---
+++ syntax/function.js
@@ -0,0 +1,27 @@
+//<함수>
+//함수는 프로그램에 대해 이름을 붙이는것과 같다
+//함수를 사용하면 정확하게 똑같은 코드라고 확신할 수 있다
+//유지보수의 편리성
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+f123();
+console.log('U');
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+f123();
+ 
+function f123(){
+  console.log(1);
+  console.log(2);
+  console.log(3);
+  console.log(4);
+}
