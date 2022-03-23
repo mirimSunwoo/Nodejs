e5a8f3abc7cc7ad66ebc243d5c7e89fd082103c5
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2022-03-23 19:17:12 +0900

    함수의 입력

---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+//round는 반올림을 해주는 함수이다
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first,second){ //parameter(매개변수)
+    console.log(first+second);
+}
+
+sum(2,4); //argument
